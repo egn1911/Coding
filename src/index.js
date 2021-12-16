427acf4126b20bca20ee5a346399361d4d76ab38
egn1911 <earth6341@gmail.com>
2021-12-17 00:47:11 +0700

    gg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-//6.55
+//const text = 'the queens gambit'
+
+function capitalise(text){
+    const texts = text.split(' ');
+    newText = texts.map(texts => texts.substr(0,1).toUpperCase().concat(texts.substr(1)))
+    return newText.join(' ');
+}
+
+console.log(capitalise('king is the king'))
