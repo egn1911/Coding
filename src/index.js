67112c8e016dcd952d16cdfb06e251fb8d6e4f67
EpSero <earth6341@gmail.com>
2022-04-18 00:11:27 +0700

    EventListener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-//const text = 'the queens gambit'
-
-function capitalise(text){
-    const texts = text.split(' ');
-    newText = texts.map(texts => texts.substr(0,1).toUpperCase().concat(texts.substr(1)))
-    return newText.join(' ');
+const hexagon = document.querySelector('.avatar')
+let width = hexagon.style.width
+function alertMe(){
+    width += 20
+    hexagon.style.top = width + 'px'
+    console.log('clicked')
 }
 
-console.log(capitalise('king is the king'))
+hexagon.addEventListener('click',alertMe)
